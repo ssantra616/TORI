d50d7941b5073469c0abfd44de6ff33564817e71
ssantra616 <ssantra@charlotte.edu>
2025-11-08 19:08:59 -0500

    Change comments to single-line format in TourScreen
    Updated comments in TourScreen.tsx to use single-line format.

--- src/screens/TourScreen.tsx
+++ src/screens/TourScreen.tsx
@@ -1,6 +1,7 @@
-## Will have the VR 
-## the voice over 
-## the map 
+// Will have the VR 
+// the voice over 
+// the map 
+
 import { View, Text, StyleSheet } from "react-native";
 export default function TourScreen() {
   return (
